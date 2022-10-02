4d779e48a7d510e10a54ebca13d060bbef21d3ba
elvis.sekularac <elvis.sekularac@ensiie.fr>
2022-10-02 14:43:06 +0200

    IAS-4 : Creation du component percentage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PercentageComponent } from './percentage/percentage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PercentageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/event.model.ts
@@ -0,0 +1,155 @@
+export interface Event {
+  kind: 'calendar#event';
+  etag: string;
+  id: string;
+  status: string;
+  htmlLink: string;
+  created: string;
+  updated: string;
+  summary: string;
+  description: string;
+  location: string;
+  colorId: string;
+  creator: {
+    id: string;
+    email: string;
+    displayName: string;
+    self: boolean;
+  };
+  organizer: {
+    id: string;
+    email: string;
+    displayName: string;
+    self: boolean;
+  };
+  start: {
+    date: string; // Only if "whole day" event
+    dateTime: string; // Only if not "whole day" event
+    timeZone: string;
+  };
+  end: {
+    date: string;
+    dateTime: string;
+    timeZone: string;
+  };
+  endTimeUnspecified: boolean;
+  recurrence: [string];
+  recurringEventId: string;
+  originalStartTime: {
+    date: string;
+    dateTime: string;
+    timeZone: string;
+  };
+  transparency: string;
+  visibility: string;
+  iCalUID: string;
+  sequence: number;
+  attendees: [
+    {
+      id: string;
+      email: string;
+      displayName: string;
+      organizer: boolean;
+      self: boolean;
+      resource: boolean;
+      optional: boolean;
+      responseStatus: string;
+      comment: string;
+      additionalGuests: number;
+    }
+  ];
+  attendeesOmitted: boolean;
+  extendedProperties: {
+    private: {
+      (key): string;
+    };
+    shared: {
+      (key): string;
+    };
+  };
+  hangoutLink: string;
+  conferenceData: {
+    createRequest: {
+      requestId: string;
+      conferenceSolutionKey: {
+        type: string;
+      };
+      status: {
+        statusCode: string;
+      };
+    };
+    entryPoints: [
+      {
+        entryPointType: string;
+        uri: string;
+        label: string;
+        pin: string;
+        accessCode: string;
+        meetingCode: string;
+        passcode: string;
+        password: string;
+      }
+    ];
+    conferenceSolution: {
+      key: {
+        type: string;
+      };
+      name: string;
+      iconUri: string;
+    };
+    conferenceId: string;
+    signature: string;
+    notes: string;
+  };
+  gadget: {
+    type: string;
+    title: string;
+    link: string;
+    iconLink: string;
+    width: number;
+    height: number;
+    display: string;
+    preferences: {
+      (key): string;
+    };
+  };
+  anyoneCanAddSelf: boolean;
+  guestsCanInviteOthers: boolean;
+  guestsCanModify: boolean;
+  guestsCanSeeOtherGuests: boolean;
+  privateCopy: boolean;
+  locked: boolean;
+  reminders: {
+    useDefault: boolean;
+    overrides: [
+      {
+        method: string;
+        minutes: number;
+      }
+    ];
+  };
+  source: {
+    url: string;
+    title: string;
+  };
+  attachments: [
+    {
+      fileUrl: string;
+      title: string;
+      mimeType: string;
+      iconLink: string;
+      fileId: string;
+    }
+  ];
+  eventType: string;
+}
+
+export class EventList {
+  constructor(calendarId: string, eventList: Event[]){
+    this.calendarId = calendarId
+    this.eventList = eventList
+  }
+
+  calendarId: string
+  eventList: Event[]
+}
