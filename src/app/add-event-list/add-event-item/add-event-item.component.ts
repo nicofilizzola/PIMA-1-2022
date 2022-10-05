926d9ffc9e5dbc943b65266e49acfb9320fa190c
nicofilizzola <71346630+nicofilizzola@users.noreply.github.com>
2022-10-05 23:43:46 +0200

    ICP-2 : Interface 'options avancées' dans ajout d'événements (#6)
    * ICP-2 : Création du bouton 'options avancées'
    
    * ICP-2 : Implémentation de l'input description, localisation, nombre d'instances et toggle 'peuvent-ils se suivre ?' (template)
    
    * ICP-2 : Tous les champs crées dans la formulaire (template)
    
    * ICP-2 : Réglage d'id des toggle switch
    
    * ICP- Gestion des inputs dynamiques (enabling et disabling en fonction des autres)
    
    * ICP-2 : Changements layout et création des méthodes getMin et getMax instances
    
    * ICP-2 : Ajout de fonctionnalité 'cacher options avancées'
    
    * ICP-2 : Paramétrage du total d'instances pour disabling du toggle d'instances consecutives
    
    * ICP-2 : Changements de layout
    
    * ICP-2 : Changements de layout

--- src/app/add-event-list/add-event-item/add-event-item.component.ts
+++ src/app/add-event-list/add-event-item/add-event-item.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
 @Component({
   selector: 'app-add-event-item',
   templateUrl: './add-event-item.component.html',
@@ -9,8 +8,43 @@ export class AddEventItemComponent {
   @Input() itemId;
   @Input() isDeletable;
   @Output() deleteItem = new EventEmitter<number>();
+  fixedEvent = false;
+  consecutiveInstances = false;
+  instanceTotal = 1;
+  advancedOptionsActive = false;
 
   onDeleteItem() {
     this.deleteItem.emit(this.itemId);
   }
+
+  onToggleFixedEvent() {
+    this.fixedEvent = !this.fixedEvent;
+
+    if (this.consecutiveInstances) {
+      this.consecutiveInstances = false;
+    }
+  }
+
+  setAdvancedOptionsActive(advancedOptionsActive: boolean) {
+    this.advancedOptionsActive = advancedOptionsActive;
+  }
+
+  setInstanceTotal(instanceTotal: number){
+    this.instanceTotal = instanceTotal
+    console.log(this.instanceTotal)
+  }
+
+  isConsecutiveInstancesInputDisabled(){
+    return this.instanceTotal < 2 || this.fixedEvent
+  }
+
+  /**
+   * @TODO
+   */
+  getMaxInstancesPerDay() {}
+
+  /**
+   * @TODO
+   */
+  getMinInstancesPerDay() {}
 }
