b27c5710cfd37f0b73971f1363998110b87d536f
Nicolas Filizzola <nicofilizzola.prod@gmail.com>
2022-10-12 10:17:06 +0200

    MET-5 : Gestion de la subscription authenticatedUserEmail au niveau de AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { GapiService } from './shared/services/gapi-service/gapi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private _subscriptions: Subscription[] = [];
 
   constructor(private readonly _gapiService: GapiService) {}
 
   ngOnInit(): void {
-    this._gapiService.authenticatedUserEmail.subscribe((authenticatedUserEmail: string) => {
-      if (authenticatedUserEmail === null) { window.location.reload() }
-    })
+    this._subscriptions.push(
+      this._gapiService.authenticatedUserEmail.subscribe(
+        (authenticatedUserEmail: string) => {
+          if (authenticatedUserEmail === null) {
+            window.location.reload();
+          }
+        }
+      )
+    );
   }
 
-  onLogout(){
-    this._gapiService.logOut()
+  ngOnDestroy(): void {
+    for (let subscription of this._subscriptions) {
+      subscription.unsubscribe()
+    }
   }
 
+  onLogout() {
+    this._gapiService.logOut();
+  }
 }
