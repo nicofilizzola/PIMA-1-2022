49f29043480d65a2cdc38b4426843ad559731984
yanis.feuvre <yanis.feuvre@ensiie.fr>
2022-10-19 16:19:06 +0200

    Ajout du component calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { NgChartsModule } from 'ng2-charts';
 import { AddEventListComponent } from './add-event-list/add-event-list.component';
 import { AddEventItemComponent } from './add-event-list/add-event-item/add-event-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PieChartComponent,
     AddEventListComponent,
-    AddEventItemComponent
+    AddEventItemComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
