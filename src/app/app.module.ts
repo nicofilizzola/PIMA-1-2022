9dca598e8661eae6b58ff01cf228b398e4966268
yanis.feuvre <yanis.feuvre@ensiie.fr>
2022-10-20 14:53:08 +0200

    Ajout component Calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,6 +25,9 @@ import { CalendarComponent } from './calendar/calendar.component';
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions, defineFullCalendarElement } from '@fullcalendar/web-component';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+
+// make the <full-calendar> element globally available by calling this function at the top-level
+defineFullCalendarElement();
 
 @Component({
   selector: 'app-calendar',
@@ -11,5 +17,13 @@ export class CalendarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,dayGridWeek,dayGridDay'
+    }
+  };
 }
