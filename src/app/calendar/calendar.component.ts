1e722928cf9165d037442ddfd52cdb144f65e61c
yanis.feuvre <yanis.feuvre@ensiie.fr>
2022-10-22 16:24:59 +0200

    Ajout gestion des events recurrents

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -5,6 +5,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import { calendarListEvents } from 'src/fixtures/fixtures';
 import rrulePlugin from '@fullcalendar/rrule'
 import { start } from '@popperjs/core';
+import { GapiService } from '../shared/services/gapi/gapi.service';
 
 
 // make the <full-calendar> element globally available by calling this function at the top-level
@@ -50,7 +51,60 @@ export class CalendarComponent implements OnInit {
     this.setupEvents();
   }
 
-  
+  getTimeString(str) {
+    return str.substring(str.indexOf("T") + 1);
+  }
+
+  getRecurr(str) {
+    
+    var recurringDays = [];
+
+    var split_string = str.split(/;|:/);
+    
+    var type_recurr= split_string.find(x=> x.substring(0,5) == "FREQ=");
+
+    if (type_recurr == "FREQ=DAILY") {
+      recurringDays = [1,2,3,4,5,6,7];
+    }
+    else {
+
+    var rec =  split_string.find(x=> x.substring(0,5) == "BYDAY");
+    var days = rec.split(",");
+
+
+    if(days !== undefined) {
+    days.forEach(day => {
+      switch(day) {
+        case "MO":
+          recurringDays.push(1);
+          break;
+        case "TU":
+          recurringDays.push(2);
+          break;
+        case "WE":
+          recurringDays.push(3);
+          break;
+        case "TH":
+          recurringDays.push(4);
+          break;
+        case "FR":
+          recurringDays.push(5);
+          break;
+        case "SA":
+          recurringDays.push(6);
+          break;
+        case "SU":
+          recurringDays.push(7);
+          break;
+        default:
+          break;
+      }
+      
+    });
+  }
+    }
+    return recurringDays;
+  }
 
   setupEvents() {
     
@@ -63,25 +117,23 @@ export class CalendarComponent implements OnInit {
 
       if ('items' in calendarContent) {
         for (const event of (calendarContent.items)){
-              
+          
           if( 'dateTime' in event.start && 'dateTime' in event.end) {
             if( 'recurrence' in event) {
+              console.log(event.summary);
               events.push({ // this object will be "parsed" into an Event Object
                 title: event.summary, // a property!
-                start: event.start.dateTime, // a property!
-                end: event.end.dateTime, 
-                rrule: 'DTSTART:'+event.start.dateTime + '\n' + event.recurrence,
-                color: '#378006',
+                daysOfWeek: this.getRecurr(event.recurrence[0]),
+                startTime: this.getTimeString(event.start.dateTime), // a property!
+                endTime: this.getTimeString(event.end.dateTime), 
               }
             );
-            console.log( event.summary + ": \n avec:"+ 'DTSTART:'+event.start.dateTime + '\n' + event.recurrence+ "Apparemment c'est: " + event.start.dateTime );        
             }
             else {
               events.push({ // this object will be "parsed" into an Event Object
                 title: event.summary, // a property!
                 start: event.start.dateTime, // a property!
                 end: event.end.dateTime, // a property! ** see important note below about 'end' **
-                color: '#378006',
               }
             );    
             }
