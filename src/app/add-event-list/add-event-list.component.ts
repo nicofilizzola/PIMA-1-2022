0e35880a6baed1440fcdba64397eb10beda7fa90
ElvisSekularac <114339964+ElvisSekularac@users.noreply.github.com>
2022-10-25 20:27:50 +0200

    ICP-6 : Supprimer tous les évènements (#12)
    * ICP-6 : Termine
    
    * Légères corrections
    
    * ICP-6 : Corrections infimes
    
    * review mise à jour (esthétique)
    
    * ICP-6 : Fonctionnalité ne pas suprimmer le premier item
    
    Co-authored-by: elvis.sekularac <elvis.sekularac@ensiie.fr>
    Co-authored-by: Nicolas Filizzola <nicofilizzola.prod@gmail.com>

--- src/app/add-event-list/add-event-list.component.ts
+++ src/app/add-event-list/add-event-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
 import { BoundsCheckerService } from '../shared/services/bounds-checker/bounds-checker.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-event-list',
@@ -13,7 +13,7 @@ export class AddEventListComponent implements OnInit {
   higher = '18:00';
   errorMessageOn = false;
 
-  constructor(private _boundsCheckerService: BoundsCheckerService) {}
+  constructor(private _boundsCheckerService: BoundsCheckerService, private modalService : NgbModal) { }
 
   ngOnInit(): void {}
 
@@ -41,7 +41,19 @@ export class AddEventListComponent implements OnInit {
       : true;
   }
 
-  isItemsLengthGreaterThan1() {
-    return this.items.length > 1;
+  onClear(){
+    const lastItem = this.items[this.items.length - 1]
+    this.items = [lastItem + 1]
+  }
+
+  onOpenClearModal(targetModal){
+    this.modalService.open(targetModal, {
+      backdrop: 'static',
+      size: 'lg'
+    });
+  }
+
+  isItemsLengthGreaterThan1(){
+    return this.items.length > 1
   }
 }
