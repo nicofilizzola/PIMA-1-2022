ddefb7aac67f1e640d38d780ab54177c78102d0f
yanis.feuvre <yanis.feuvre@ensiie.fr>
2022-10-26 14:47:10 +0200

    Utilisation de l'api gcal pour obtenir la liste des calendriers et de leurs événements

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,17 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions, defineFullCalendarElement } from '@fullcalendar/web-component';
+import { CalendarOptions, defineFullCalendarElement, DateSelectArg } from '@fullcalendar/web-component';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import { calendarListEvents } from 'src/fixtures/fixtures';
+import { Calendar } from '@fullcalendar/core';
 import rrulePlugin from '@fullcalendar/rrule'
-import { start } from '@popperjs/core';
 import { GcalService } from '../shared/services/gcal/gcal.service';
 import { CalendarList, CalendarListEntry } from '../models/calendar-list.model'
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { ONE_DAY_AGO, ONE_MONTH_AGO, ONE_WEEK_AGO } from 'src/app/constants';
 import { Event, EventList } from 'src/app/models/event.model';
-import { CalendarListListResponse } from 'src/app/models/gcal-response/calendar-list/calendar-list.list.model';
-import { EventListResponse } from 'src/app/models/gcal-response/event/event.list.model';
 
 // make the <full-calendar> element globally available by calling this function at the top-level
 defineFullCalendarElement();
@@ -24,8 +20,12 @@ defineFullCalendarElement();
 
 export class CalendarComponent implements OnInit {
   
-    
-    calendarOptions: CalendarOptions = {
+
+  //Array of all events to display on the calendar
+  events = [];
+
+  //calendarOptions used to create the calendar
+  calendarOptions: CalendarOptions = {
     plugins: [ timeGridPlugin, dayGridPlugin, rrulePlugin],
     headerToolbar: {
       left: 'prev,next today',
@@ -38,28 +38,39 @@ export class CalendarComponent implements OnInit {
     selectable: true,
     selectMirror: true,
     dayMaxEvents: true,
-    events: [
-      { // this object will be "parsed" into an Event Object
-        title: 'The Title', // a property!
-        start: '2022-10-20T13:30:00+02:00', // a property!
-        end: '2022-10-20T15:00:00+02:00', // a property! ** see important note below about 'end' **
-        color: '#378006',
-      }
-    ],
+    events: this.events,
+    
   };
-
-
   constructor(private gcal : GcalService) { }
 
   ngOnInit(): void {
 
-    this.setupEvents();
+    this.setUpCalendar();
   }
 
+  /**
+   * 
+   * @param str String respecting ISO 8601 date and time
+   * @returns string containing only time
+   */
   getTimeString(str) {
     return str.substring(str.indexOf("T") + 1);
   }
 
+  /**
+   * 
+   * @param str String respecting RRULE format
+   * @returns date after "UNTIL=" if it exists. Otherwise the result is undefined
+   * */
+  getEndRecurr(str) {
+    return (str.substring(
+      str.indexOf("UNTIL=") + 6, 
+      str.lastIndexOf("T")));
+  }
+  /**
+   * @param str String respecting RRULE format
+   * @returns array containing ints corresponding to days of recurrence from a RRULE string
+   *        (Sunday --> 0, Monday --> 1, ..., Saturday --> 6) */
   getRecurr(str) {
     
     var recurringDays = [];
@@ -111,47 +122,70 @@ export class CalendarComponent implements OnInit {
     return recurringDays;
   }
 
-  setupEvents() {
+  setUpCalendar() {
 
+    //Array of calendar's id that are already taken into account 
+    var calendarIdListSeen = [];
 
+    //Subscribing to the calendarList
+    this.gcal.calendarList$.subscribe( (_calendarList: CalendarList) => {
+      _calendarList.forEach( calendar => {
+        this.gcal.fetchCalendarEvents(calendar.id,ONE_MONTH_AGO);
+      })
+      } );
 
-    var events = [];
-    for (const fields of Object.entries(calendarListEvents)) {
+    //Subscribing to the eventList
+    this.gcal.eventList$.subscribe ( (_eventList: EventList) => {
+      if (_eventList != null) {
 
-      var calendarName = fields[0];
-      var calendarContent = fields[1];
+        for (const fields of Object.entries(_eventList) ){
 
+          var calendarId = fields[0];
+          if (!calendarIdListSeen.includes(calendarId)) 
+          {
+            calendarIdListSeen.push(calendarId);
 
-      if ('items' in calendarContent) {
-        for (const event of (calendarContent.items)){
-          
-          if( 'dateTime' in event.start && 'dateTime' in event.end) {
+            var eventlist = fields[1];
 
-            if( 'recurrence' in event) {
-              events.push({ // this object will be "parsed" into an Event Object
-                title: event.summary, 
-                daysOfWeek: this.getRecurr(event.recurrence[0]),
-                startTime: this.getTimeString(event.start.dateTime),
-                endTime: this.getTimeString(event.end.dateTime), 
-                startRecur: event.start.dateTime,
+            eventlist.forEach( _event => 
+            {
+              if(_event["start"]["dateTime"] != undefined && _event["end"]["dateTime"]) 
+              {
+                
+                if(_event["recurrence"]!=undefined) 
+                {
+                  this.events.push({
+                    title: _event["summary"],
+                    daysOfWeek: this.getRecurr(_event["recurrence"][0]),
+                    startTime: this.getTimeString(_event["start"]["dateTime"]),
+                    endTime: this.getTimeString(_event["end"]["dateTime"]),
+                    startRecur: _event["start"]["dateTime"],
+                    endRecur: this.getEndRecurr(_event["recurrence"][0]),
+                  })
+                }
+                else 
+                {
+                  this.events.push({ 
+                    title: _event["summary"], 
+                    start: _event["start"]["dateTime"], 
+                    end: _event["end"]["dateTime"], 
+                  }
+                );    
+                }
               }
-            );
-            }
-            else {
-              events.push({ // this object will be "parsed" into an Event Object
-                title: event.summary, 
-                start: event.start.dateTime, 
-                end: event.end.dateTime, 
-              }
-            );    
-            }
-        
-          }
+            })
+            
+          } 
         }
-      }
 
+        this.calendarOptions.events = this.events;
+        let calendar = new Calendar(document.getElementById("calendar"), this.calendarOptions);
+        calendar.render();
     }
-    this.calendarOptions.events = events;
+
+  })
+
+  this.gcal.fetchCalendarList();
   }
 
 
