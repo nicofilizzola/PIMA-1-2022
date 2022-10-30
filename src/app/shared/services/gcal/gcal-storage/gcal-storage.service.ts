c32359938da962dc2306b91f311ff2cf10d10a6a
Nicolas Filizzola <nicofilizzola.prod@gmail.com>
2022-10-30 19:47:27 +0100

    MET-8 : Création des fonctions getEventList, getCalendarEventList et getCalendarList

--- src/app/shared/services/gcal/gcal-storage/gcal-storage.service.ts
+++ src/app/shared/services/gcal/gcal-storage/gcal-storage.service.ts
@@ -1,18 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ONE_DAY_AGO, ONE_MONTH_AGO, ONE_WEEK_AGO } from 'src/app/constants';
 import { CalendarList } from 'src/app/models/calendar-list.model';
 import { EventList, EventListEntry, Event } from 'src/app/models/event.model';
-
-enum TimeMinFlag {
-  ONE_DAY_AGO,
-  ONE_WEEK_AGO,
-  ONE_MONTH_AGO,
-}
-enum TimeMaxFlag {
-  ONE_DAY_FROM_TODAY,
-  ONE_WEEK_FROM_TODAY,
-  ONE_MONTH_FROM_TODAY,
-}
 
 @Injectable({
   providedIn: 'root',
@@ -31,11 +21,16 @@ export class GcalStorageService {
     return this.calendarList$.getValue();
   }
 
+  /**
+   * @note Gets all of the fetched events from the specified calendar in the desired time ranged (if specified)
+   * @note Both time parameters `timestampMin` and `timestampMax` can use constants ONE_DAY_AGO, ONE_WEEK_AGO, ONE_MONTH_AGO,
+   * TODAY, ONE_DAY_FROM_TODAY, ONE_WEEK_FROM_TODAY, ONE_MONTH_FROM_TODAY
+   */
   getCalendarEventList(
     calendarId: string,
-    timeMin?: TimeMinFlag,
-    timeMax?: TimeMaxFlag
-  ): Array<Event> {
+    timestampMin?: number,
+    timestampMax?: number
+  ): Event[] {
     let eventListEntries = Object.entries(this.eventList$.getValue());
     let calendarEventListEntry: EventListEntry[] = eventListEntries.filter(
       (eventListEntry: EventListEntry) => eventListEntry[0] === calendarId
@@ -47,10 +42,56 @@ export class GcalStorageService {
       );
       return;
     }
-    return calendarEventListEntry[0][1];
+
+    let calendarEvents = calendarEventListEntry[0][1];
+    let rangedEventList = this._getRangedEventList(
+      calendarEvents,
+      timestampMin,
+      timestampMax
+    );
+    return rangedEventList;
   }
 
-  getEventList(timeMin?: TimeMinFlag, timeMax?: TimeMaxFlag): EventList {
-    let eventList = this.eventList$.getValue();
+  /**
+   * @note Gets all of the fetched events from all the calendars in the desired time ranged (if specified)
+   * @note Both time parameters `timestampMin` and `timestampMax` can use constants ONE_DAY_AGO, ONE_WEEK_AGO, ONE_MONTH_AGO,
+   * TODAY, ONE_DAY_FROM_TODAY, ONE_WEEK_FROM_TODAY, ONE_MONTH_FROM_TODAY
+   */
+  getEventList(timestampMin?: number, timestampMax?: number): EventList {
+    let eventListEntries = Object.entries(this.eventList$.getValue());
+    let rangedEventListEntries = eventListEntries.map(
+      (eventListEntry: EventListEntry) => {
+        return this._getRangedEventList(
+          eventListEntry[1],
+          timestampMin,
+          timestampMax
+        );
+      }
+    );
+    let rangedEventList = Object.fromEntries(rangedEventListEntries);
+    return rangedEventList;
+  }
+
+  private _getRangedEventList(
+    calendarEvents: Event[],
+    timestampMin?: number,
+    timestampMax?: number
+  ): Event[] {
+    let filteredEvents = [...calendarEvents];
+
+    if (timestampMin) {
+      filteredEvents = calendarEvents.filter((event: Event) => {
+        const eventStartTimestamp = new Date(event.start.dateTime).getTime();
+        return timestampMin < eventStartTimestamp;
+      });
+    }
+    if (timestampMax) {
+      filteredEvents = calendarEvents.filter((event: Event) => {
+        const eventEndTimestamp = new Date(event.end.dateTime).getTime();
+        return timestampMax > eventEndTimestamp;
+      });
+    }
+
+    return filteredEvents;
   }
 }
