ad64b24f13edc7092ff913148174d4a25456e87d
Nicolas Filizzola <nicofilizzola.prod@gmail.com>
2022-10-31 18:14:29 +0100

    MET-8 : Fixed percentage service

--- src/app/shared/services/percentage/percentage.service.ts
+++ src/app/shared/services/percentage/percentage.service.ts
@@ -1,51 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Event, EventList, EventListEntry } from 'src/app/models/event.model';
-import { GcalService } from '../gcal/gcal.service';
+import { GcalStorageService } from '../gcal/gcal-storage/gcal-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PercentageService {
+  private _fetchedEvents: EventList;
   totalEventTime: number;
 
-  constructor(private _gcalService: GcalService) {
-    // this._gcalService.eventList$.subscribe((eventList: EventList) => {
-    //   if (this._gcalService.eventList$.getValue() != null) {
-    //     let events = Object.entries(eventList)
-    //       .map((eventListEntry: EventListEntry) => eventListEntry[1])
-    //       .flat();
-    //     this._setTotalEventsTime(events);
-    //   }
-    // });
+  constructor(private _gcalStorageService: GcalStorageService) {}
+
+  public initialize() {
+    this._fetchedEvents = this._gcalStorageService.getEventList();
+
+    this._setTotalEventTime(
+      Object.entries(this._fetchedEvents)
+        .map((eventListEntry: EventListEntry) => eventListEntry[1])
+        .flat()
+    );
   }
 
   /**
    * @returns The calendar's events time percentage in the total event time. The result is rounded to two decimals
    */
   public getCalendarPercentage(calendarId: string): number {
-    if (
-      this.totalEventTime == null ||
-      this._gcalService.eventList$.getValue() == null
-    ) {
+    if (this.totalEventTime == null) {
       return 0;
     }
 
     let calendarEventsTotalTime = 0;
-    let calendarEvents = this._gcalService.eventList$.getValue()[calendarId];
+    let calendarEvents = this._fetchedEvents[calendarId];
 
     for (let event of calendarEvents) {
       calendarEventsTotalTime += this._getEventDuration(event);
     }
 
-    const ROUNDING_COEFFICIENT = 100;
-    const longPercentage =
-      (calendarEventsTotalTime / this.totalEventTime) * 100;
-    const roundedPercentage =
-      Math.round(longPercentage * ROUNDING_COEFFICIENT) / ROUNDING_COEFFICIENT;
-    return roundedPercentage;
+    let percentage = this._getRoundedPercentage(
+      (calendarEventsTotalTime / this.totalEventTime) * 100,
+      100
+    );
+    return percentage;
   }
 
-  private _setTotalEventsTime(events: Event[]) {
+  private _setTotalEventTime(events: Event[]) {
     let totalTime = 0;
 
     for (let event of events) {
@@ -56,6 +54,7 @@ export class PercentageService {
 
   /**
    * @returns The event's duration in seconds
+   * @see this._setTotalEventTime
    */
   private _getEventDuration(event: Event): number {
     const SECOND_IN_TIMESTAMP_FORMAT = 1000;
@@ -65,5 +64,42 @@ export class PercentageService {
       (eventEndDate.getTime() - eventStartDate.getTime()) /
       SECOND_IN_TIMESTAMP_FORMAT
     );
+  }
+
+  /**
+   * @brief Rounds `longPercentage` to number of 0s in `roundingCoefficient` (ex: `roundingCoefficient = 100`, rounded to 2 decimals)
+   * @param roundingCoefficient must be a power of 10
+   * @see this.getCalendarPercentage
+   */
+  private _getRoundedPercentage(
+    longPercentage: number,
+    roundingCoefficient: number
+  ): number {
+    if (
+      parseInt(roundingCoefficient.toString().charAt(0)) !== 1 ||
+      !this._allDigitsButFirstEqualZero(roundingCoefficient)
+    ) {
+      console.error(
+        'The provided roundingCoefficient is invalid. It should be a power of ten (ex: 10, 100, 1000, ...)'
+      );
+      return;
+    }
+
+    return (
+      Math.round(longPercentage * roundingCoefficient) / roundingCoefficient
+    );
+  }
+
+  /**
+   * @see this._getRoundedPercentage
+   */
+  private _allDigitsButFirstEqualZero(number: number) {
+    const digits = number.toString().split('');
+    for (let i = 1; i < digits.length; i++) {
+      if (parseInt(digits[i]) !== 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
