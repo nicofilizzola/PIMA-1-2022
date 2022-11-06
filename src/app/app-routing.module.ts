02e38f51662afe303dfab3368900a07616612976
yanis.feuvre <yanis.feuvre@ensiie.fr>
2022-11-06 15:39:44 +0100

    Ajout du calendar sur la page d'accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddEventListComponent } from './components/add-event-list/add-event-list.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 
 
 const routes: Routes = [
   { path: 'evenements', component: AddEventListComponent},
-  { path: 'statistiques', component: PieChartComponent }
+  { path: 'statistiques', component: PieChartComponent },
+  { path: '', component: CalendarComponent },
+  
 ];
 
 @NgModule({
