00b25961191394aa0df1ae1de33e3104e366938a
gozipizi <quentin.gourdel@ensiie.fr>
2022-11-24 18:22:19 +0100

    MET-11 Creation du plan

--- src/app/shared/services/calendar-generator/calendar-generator.service.ts
+++ src/app/shared/services/calendar-generator/calendar-generator.service.ts
@@ -12,6 +12,14 @@ export class CalendarGeneratorService {
   constructor(private readonly _gcalStorageService : GcalStorageService,
     private _httpService : GcalHttpService) { }
 
+    //Recuperer une liste de constraint event, 
+    //La parcourir 
+      //Pour chaque elt, on regarde sa durée, on cherche le premier slot dans le availableTimeSlots
+      //Dès qu'on la, on crée un event, à partir de tout ça
+      //On l'envoie dans le cal
+      //On met a jour les time slots 
+      //Puis on reboucle 
 
+    //On met a jour le storage. 
 
 }
